e0d021e6b65bf113575759a08b211b1ef7bd5b24
nomatteus <matt@ruten.ca>
2013-03-17 20:44:01 -0400

    Add HTML5 mobile boilerplate, some sample content, and placeholders.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "http://rubygems.org"
 
-gem "jekyll",           "~>0.12.1"
+gem "jekyll",                 "~>0.12.1"
+gem "jekyll-asset-pipeline",  "~>0.1.6"
+gem "coffee-script",          "~>2.2.0"
+gem "sass",                   "~>3.2.7"
---
+++ _plugins/jekyll_asset_pipeline.rb
@@ -0,0 +1,27 @@
+require 'jekyll_asset_pipeline'
+
+module JekyllAssetPipeline
+  class CoffeeScriptConverter < JekyllAssetPipeline::Converter
+    require 'coffee-script'
+
+    def self.filetype
+      '.coffee'
+    end
+
+    def convert
+      return CoffeeScript.compile(@content)
+    end
+  end
+
+  class SassConverter < JekyllAssetPipeline::Converter
+    require 'sass'
+
+    def self.filetype
+      '.scss'
+    end
+
+    def convert
+      return Sass::Engine.new(@content, syntax: :scss).render
+    end
+  end
+end
