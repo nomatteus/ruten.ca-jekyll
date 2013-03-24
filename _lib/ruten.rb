2fb322c2411b9c60c2127d9208308d0d19e2972d
nomatteus <matt@ruten.ca>
2013-03-24 00:10:51 -0400

    Style fix and add random script file

---
+++ _lib/ruten.rb
@@ -0,0 +1,10 @@
+# A place to put random scripts to beat my blog into shape.
+
+
+# Dir.foreach("./") do |fname|
+#   puts fname
+# end
+
+# Dir.glob("**/*.jpg") do |my_text_file| # note one extra "*"
+#   puts "working on: " + my_text_file + "... "
+# end
